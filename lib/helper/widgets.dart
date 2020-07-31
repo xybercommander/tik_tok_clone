570d20855f6769324b9b0d8ca3ba8b195b4c3a53
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-07-31 18:26:08 +0530

    added new files

--- lib/helper/widgets.dart
+++ lib/helper/widgets.dart
@@ -34,3 +34,107 @@ Widget accountThumnails(context, int likes, String pic) {
     ),
   );
 }
+
+//this is for the trending hashtags in search
+Widget trendingTag(String hashtag, String likes) {
+  return Container(
+    width: double.infinity,
+    height: 60,
+    color: Colors.black87,
+    padding: EdgeInsets.all(16),
+    child: Row(
+      children: [
+        Container(
+          height: 40,
+          width: 30,
+          decoration: BoxDecoration(
+              color: Colors.grey[800], borderRadius: BorderRadius.circular(40)),
+          child: Center(
+            child: Text(
+              "#",
+              style: TextStyle(
+                  fontFamily: "Quicksand", fontSize: 20, color: Colors.yellow),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "$hashtag",
+              style: TextStyle(
+                  color: Colors.white, fontSize: 12, fontFamily: "Quicksand"),
+            ),
+            Text(
+              "Trending Hashtag",
+              style: TextStyle(color: Colors.grey[600], fontSize: 10),
+            )
+          ],
+        ),
+        Spacer(),
+        Container(
+          height: 30,
+          width: 50,
+          decoration: BoxDecoration(
+              color: Colors.grey, borderRadius: BorderRadius.circular(40)),
+          child: Center(
+            child: Text(
+              "$likes",
+              style: TextStyle(
+                  fontFamily: "Quicksand", fontSize: 13, color: Colors.white),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+// this is for the list in the search page
+Widget thumbnailList(
+    context, String pic1, String pic2, String pic3, String pic4) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 200,
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        Container(
+          height: 50,
+          width: MediaQuery.of(context).size.width / 3 - 15,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage("assets/$pic1"), fit: BoxFit.cover),
+              border: Border.all(color: Colors.black, width: 1)
+              ),
+        ),
+        Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width / 3 - 15,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage("assets/$pic2"), fit: BoxFit.cover),
+              border: Border.all(color: Colors.black, width: 1)
+            ),
+        ),
+        Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width / 3 - 15,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage("assets/$pic3"), fit: BoxFit.cover),
+              border: Border.all(color: Colors.black, width: 1)
+              ),
+        ),
+        Container(
+          height: 200,
+          width: MediaQuery.of(context).size.width / 3 - 15,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage("assets/$pic4"), fit: BoxFit.cover),
+              border: Border.all(color: Colors.black, width: 1)
+              ),
+        ),
+      ],
+    ),
+  );
+}
--- lib/views/search.dart
+++ lib/views/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intern_demo/helper/widgets.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -9,7 +10,21 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Search"),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 4,
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/searchBg.jpg",), fit: BoxFit.cover)
+              ),
+            ),
+            trendingTag("welcome2020_india", "400k"),
+            thumbnailList(context, "itachi.jpg", "kakashi.jpg", "minato.jpg", "tanjiro.jpg")
+          ],
+        ),
+      ),
     );
   }
 }
